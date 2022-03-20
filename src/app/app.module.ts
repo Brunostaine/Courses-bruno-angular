a4175ec167ae1be61b1ad21ae87bf0a9b9744a51
Bruno Staine <brunostaine@hotmail.com>
2022-03-20 15:10:58 -0300

    :sparkles: Component star

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,21 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { courseListComponent } from './courses/course-list.component';
+import { StarComponent } from './star/star.component';
+
 
 @NgModule({ //decorator
   declarations: [
     AppComponent,
     courseListComponent, // está vindo do course list component
+    StarComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/courses/course-list.component.ts
+++ src/app/courses/course-list.component.ts
@@ -5,7 +5,8 @@ import { Course } from "./course";
 
 @Component({
     selector: 'app-course-list',
-    templateUrl: './course-list.component.html'
+    templateUrl: './course-list.component.html',
+    styleUrls: ['./course-list.component.css']
 })
 
 export class courseListComponent implements OnInit { // importar no modulo raiz
@@ -15,24 +16,24 @@ export class courseListComponent implements OnInit { // importar no modulo raiz
         this._courses = [
             {
                 id: 1,
-                name: 'Angular 1',
-                imageUrl: '',
-                price: 99.90,
+                name: 'Angular Forms',
+                imageUrl: '/assets/images/forms.png',
+                price: 99.99,
                 code: 'XPS-1235',
                 duration: 120,
                 rating: 4,
-                releaseDate: 'DATA',
+                releaseDate: 'Março 2022',
                 description: '',
             },
             {
                 id: 2,
-                name: 'Angular 2',
-                imageUrl: '',
-                price: 45.90,
+                name: 'Angular Http',
+                imageUrl: '/assets/images/http.png',
+                price: 49.99,
                 code: 'XPS-9587',
                 duration: 60,
                 rating: 4.5,
-                releaseDate: 'DATA',
+                releaseDate: 'Março 2022',
                 description: '',
             }
         ]
